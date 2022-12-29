ba5c763ff9bbe2b212a69a62d9f425c60a9d9926
guy <devtyl3r@gmail.com>
2022-12-29 15:40:32 -0400

    tests

--- temp.py
+++ temp.py
@@ -2,8 +2,9 @@ from src.ORM import BaseModel, Field
 
 class User(BaseModel):
     Apple = Field(required=True, type=str)
-    Pear = Field(required=False, type=str)
+    Pear = Field(required=True, type=str)
 
 user = User()
 
-user.insert(Apple="haha")
+user.insert(Apple="Wow")
+user.update(Apple="haha")
