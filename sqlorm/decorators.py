140743c6a7d34a71588b9ceeb0e3df2e43837c2a
Ross Miller <rosspkm@gmail.com>
2022-12-29 21:20:06 -0500

    small update

--- sqlorm/decorators.py
+++ sqlorm/decorators.py
@@ -1,7 +1,9 @@
-from sqlorm.drivers import DatabaseDriver
+from .drivers import DatabaseDriver
 def Database(driver: DatabaseDriver):
+    """
+    Decorator to initialize database connection within the created validation model
+    """
     def Model(cls):
         cls.database = driver
         return cls
-    
     return Model
--- sqlorm/orm.py
+++ sqlorm/orm.py
@@ -1,11 +1,17 @@
 from .validator import Validator
 
 class Field():
+    """
+    Class to denote a field on the model - if not present, the class variable is not recognized as a valid model field
+    """
     def __init__(cls, **kwargs):
         for k,v in kwargs.items():
             setattr(cls, k, v)
 
 class BaseModel():
+    """
+    Base Model inhereted by created ORM class to validate fields passed and handles connectivity to database to execute upon
+    """
     def __init__(self):
         # automatically passed from database decorator that sets self.database from driver passed
         self.database = self.database
