6e4fe5c00f670bed01be830173111f4a4fc0ce65
Ross Miller <rosspkm@gmail.com>
2022-12-30 17:47:30 -0500

    updated doc strings for postgres driver/functions and default driver

--- sqlorm/drivers.py
+++ sqlorm/drivers.py
@@ -2,17 +2,35 @@ import psycopg2
 
 
 class DatabaseDriver():
+    """
+    Default driver to handle typing for Database decorator
+    """
     ...
 
 
 class Postgres():
+    """
+    Postgres driver for connecting to a postgres database\n
+    database - postgres database name\n
+    username - username of account for postgres database\n
+    password - password of account for postgres database\n
+    host - host address for postgres database\n
+    port - port for postgres database\n
+    options - any other database options that may need to be supplied (i.e: schema, ect...)
+    """
     def __init__(self, database: str = None, username: str = None, password: str = None, host: str = None, port: int = None, options: str = None):
         self.driver = "postgres"
         self.connection = self.connect(database, username, password, host, port, options)
 
     def connect(self, database: str = None, username: str = None, password: str = None, host: str = None, port: int = None, options: str = None):
         """
-        Function to handle connections to and from the postgres database
+        Function to handle connections to and from the postgres database\n
+        database - postgres database name\n
+        username - username of account for postgres database\n
+        password - password of account for postgres database\n
+        host - host address for postgres database\n
+        port - port for postgres database\n
+        options - any other database options that may need to be supplied (i.e: schema, ect...) 
         """
         try:
             connection = psycopg2.connect(
@@ -69,6 +87,7 @@ class Postgres():
         Function to handle updating data into a postgres table\n
         schema - schema in database\n
         table - table in database\n
+        conditions - query of postgres search to find particular rows\n
         values - object of column:values to be inserted into postgres table
         """
         if not conditions:
@@ -92,7 +111,13 @@ class Postgres():
             raise psycopg2.Error(f"{error}")
 
     def find_one(self, schema: str = None, table: str = None, conditions: dict = None, select: list = None):
-
+        """
+        Function to handle finding one row from a postgres table\n
+        schema - schema in database\n
+        table - table in database\n
+        conditions - query of postgres search to find particular rows\n
+        select - columns of postgres table selecting, defaults to '*'
+        """
         if not isinstance(select, (dict, type(None))):
             raise TypeError(f"'select' is not of type: {dict} or type: {type(None)} instead found type: {type(select)}")
 
@@ -117,7 +142,13 @@ class Postgres():
             raise psycopg2.Error(f"{error}")
 
     def find_many(self, schema: str = None, table: str = None, conditions: str = None, select: tuple = '*'):
-
+        """
+        Function to handle finding many rows from a postgres table\n
+        schema - schema in database\n
+        table - table in database\n
+        conditions - query of postgres search to find particular rows\n
+        select - columns of postgres table selecting, defaults to '*'
+        """
         if not isinstance(select, (dict, type(None))):
             raise TypeError(f"'select' is not of type: {dict} or type: {type(None)} instead found type: {type(select)}")
 
@@ -142,7 +173,12 @@ class Postgres():
             raise psycopg2.Error(f"{error}")
 
     def delete_one(self, schema: str = None, table: str = None, conditions: dict = None):
-
+        """
+        Function to handle deleting one row from a postgres table\n
+        schema - schema in database\n
+        table - table in database\n
+        conditions - query of postgres search to find particular rows\n
+        """
         if not conditions:
             raise ValueError("No conditions were supplied to be found with")
 
@@ -163,7 +199,12 @@ class Postgres():
             raise psycopg2.Error(f"{error}")
 
     def delete_many(self, schema: str = None, table: str = None, conditions: dict = None):
-
+        """
+        Function to handle deleting many rows from a postgres table\n
+        schema - schema in database\n
+        table - table in database\n
+        conditions - query of postgres search to find particular rows\n
+        """
         if not conditions:
             raise ValueError("No conditions were supplied to be found with")
 
