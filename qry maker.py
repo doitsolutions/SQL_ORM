f6089d77bb858257bb5d16ef4c8a1686db387970
dragonwang007 <aosmolovsky21@gmail.com>
2023-01-08 13:38:31 -0500

    Add files via upload

--- qry maker.py
+++ qry maker.py
@@ -5,8 +5,35 @@ Created on Sun Jan  1 17:41:18 2023
 @author: bearjew
 """
 
-qry = {"over":"test","where":"table_name_col","from_table":"test part func",
-       "select": "test=val", "group_by":"go","order_by": "group_by test"}
+
+
+example_sql_values = {
+    'select' : 'col_name as alias, col_2',
+    'from_table' : 'table_name',
+    'into' : 'table_name',
+    'where' : "col_name LIKE '%soft%'",
+    'group_by' : 'col_name,col_2',
+    'order_by' : 'col_name,col_2',
+    'delete_from' : 'table_name',
+    'insert_into' :  'table_name  (col_name,col_2)',
+    'values' : "'test_val_1','test_val_2'",
+    'alter_table' : 'table_name',
+    'add_col' : 'col_name varchar(255)',
+    'drop_col' : 'col_name',
+    'rename_col' : ' col_nme to new_name;',
+    'alter_column' : 'col_name varchar(100)',
+    'modify_column' : 'col_name varchar(100)',
+    'limit' : '5',
+    'update' : 'table_name',
+    'set_val' : "col_name = 'test_val', col_2 = 'test_val_2'" 
+    }
+
+
+
+qry = {
+       'alter_table' : 'table_name',
+       'drop_col' : 'col_name',
+       }
 
 
 def sql_builder(sql_dict):
@@ -30,7 +57,6 @@ def sql_builder(sql_dict):
         "limit" : 7,
         "update": 0,
         "set_val": 1,
-        "over" : 4
         }
     
     
@@ -38,25 +64,24 @@ def sql_builder(sql_dict):
     query= ''
 
     sql_terms= {
-    'select' : 'SELECT %s',
-    'over' : 'SELECT %s , %s OVER (PARTITION BY %s) AS %s',
-    'from_table' : 'FROM %s',
-    'into' : 'INTO %s',
-    'where' : 'WHERE %s',
-    'group_by' : 'GROUP BY %s',
-    'order_by' : 'ORDER BY %s',
-    'delete_from' : 'DELETE FROM %s',
-    'insert_into' : 'INSERT INTO %s (%s)',
-    'values' : 'VALUES (%s)',
-    'alter_table' : 'ALTER TABLE %s',
-    'add_col' : 'ADD %s %s',
-    'drop_col' : 'DROP COLUMN %s;',
-    'rename_col' : 'RENAME COLUMN %s to %s;',
-    'alter_column' : 'ALTER COLUMN %s %s',
-    'modify_column' : 'MODIFY COLUMN %s %s',
-    'limit' : 'LIMIT %s',
-    'update' : 'UPDATE %s',
-    'set_val' : 'SET %s : %s'
+    'select' : 'SELECT {select}',
+    'from_table' : 'FROM {from_tbale}',
+    'into' : 'INTO {into}',
+    'where' : 'WHERE {where}',
+    'group_by' : 'GROUP BY {group_by}',
+    'order_by' : 'ORDER BY {order_by}',
+    'delete_from' : 'DELETE FROM {delete_from}',
+    'insert_into' : 'INSERT INTO {insert_into}',
+    'values' : 'VALUES ({values})',
+    'alter_table' : 'ALTER TABLE {alter_table}',
+    'add_col' : 'ADD {add_col}',
+    'drop_col' : 'DROP COLUMN {drop_col}',
+    'rename_col' : 'RENAME COLUMN {rename_col}',
+    'alter_column' : 'ALTER COLUMN {alter_column}',
+    'modify_column' : 'MODIFY COLUMN {modify_column}',
+    'limit' : 'LIMIT {limit}',
+    'update' : 'UPDATE {update}',
+    'set_val' : 'SET {set_val} '
     }
 
     user_query = {}
@@ -67,9 +92,6 @@ def sql_builder(sql_dict):
         user_query[i] = val_dict.get(i)
         if user_query[i] == None:
           return(-1)
-    if "over" in list(user_query.keys()) and "select" in list(user_query.keys()):
-       del user_query['select']
-       user_query.update({'over':0})
     
     sorted_by_val = {k:v for k,v  in sorted(user_query.items(), key= lambda v: v[1])}
     
@@ -78,7 +100,26 @@ def sql_builder(sql_dict):
         
         query = query + sql_terms.get(i) + " \n" 
         
+    sql_dict_vals = {}
+    
+    for k in sql_dict:
+        sql_dict_vals[sorted_by_val.get(k)] = sql_dict.get(k)
+        
+    sql_dict_vals_sorted = sorted(sql_dict_vals)
+ 
+    sql_dict_val_after_sort = {}
+    
+    for i in sql_dict_vals_sorted:
+        sql_dict_val_after_sort[i] = sql_dict_vals[i]    
+
+    for i in sql_dict_val_after_sort.values():
+        query = query.replace('%s', str.format(i) ,1)
+        
     return(query)
+    
+    
+test=sql_builder(qry)
+print(test)    
         
     
 
